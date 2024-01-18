2b9041f2131f374694d53468545a003a6bab7d3b
porterbrien <porterbrien@icloud.com>
2024-01-18 09:52:11 -0700

    learning 3 updated

--- prepare/Learning03/Program.cs
+++ prepare/Learning03/Program.cs
@@ -7,3 +7,35 @@ class Program
         Console.WriteLine("Hello Learning03 World!");
     }
 }
+
+// Ask for random number 
+
+
+Random randomGenerator = new Random();
+int number = randomGenerator.Next(1, 11);
+
+int random = 4;
+int guess;
+do {
+
+// Ask for user guess
+    System.Console.Write("What is your guess? ");
+    guess = int.Parse(Console.ReadLine());
+
+    // check if guess is higher
+    if (guess > random) {
+        System.Console.WriteLine("too high");
+    }
+
+    // Check if guess is lower
+    else if (guess< random) {
+        System.Console.WriteLine("Too Low");
+    }
+
+    // Check if guess is match
+    else 
+    {
+        System.Console.WriteLine("Correct");
+    }
+} while (random != guess);
+// keep going...
